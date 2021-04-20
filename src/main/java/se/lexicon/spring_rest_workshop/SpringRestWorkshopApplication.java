760709d3905b6c084ef3301f9289510f270055f7
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2021-04-20 10:16:40 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/spring_rest_workshop/SpringRestWorkshopApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.spring_rest_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/spring_rest_workshop/SpringRestWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.spring_rest_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
